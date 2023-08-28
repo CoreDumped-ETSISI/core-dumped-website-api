bab9132b01af730feef13bda88edf97ec0086bed
Rowiz49 <adrianpuyetm@gmail.com>
2023-08-28 13:18:47 +0200

    Add mongo connection and error handling
    feat: Connect to mongoDB and add error handling for all requests

--- src/app.js
+++ src/app.js
@@ -1,10 +1,27 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
 const app = express();
+
+mongoose.connect(process.env.MONGO_URI, { useMongoClient: true });
 
 app.use((req, res, next) => {
   res.status(200).json({
     message: "OK",
+  });
+});
+
+app.use((req, res, next) => {
+  const error = new Error("Not found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
   });
 });
 
