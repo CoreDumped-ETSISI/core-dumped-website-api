81a7515e3145c6e2dd74475bf5af7d24af4d193b
Rowiz49 <adrianpuyetm@gmail.com>
2023-08-29 13:06:11 +0200

    Update project.js

--- src/routes/projects.js
+++ src/routes/projects.js
@@ -1,14 +1,20 @@
 const express = require("express");
 const router = express.Router();
-const EventControllers = require("../controllers/projects");
+const ProjectControllers = require("../controllers/projects");
 
 //Publicly accessible
-router.get("/", EventControllers.projects_get_all);
-router.get("/proyecto-:projectId", EventControllers.projects_get_project);
-router.get("/categorias", EventControllers.projects_get_categories);
+router.get("/", ProjectControllers.projects_get_all);
+router.get("/proyecto-:projectId", ProjectControllers.projects_get_project);
+router.get("/categorias", ProjectControllers.projects_get_categories);
 
 //Only accesible with authorization
-router.post("/", EventControllers.projects_create_project);
-router.delete("/proyecto-:projectId", EventControllers.projects_delete_project);
-router.patch("/proyecto-:projectId", EventControllers.projects_update_project);
+router.post("/", ProjectControllers.projects_create_project);
+router.delete(
+  "/proyecto-:projectId",
+  ProjectControllers.projects_delete_project
+);
+router.patch(
+  "/proyecto-:projectId",
+  ProjectControllers.projects_update_project
+);
 module.exports = router;
