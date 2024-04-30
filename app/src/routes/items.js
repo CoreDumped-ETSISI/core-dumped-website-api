70dd05631ced8ae3758959fc98455227757cce17
programmer-panda-cute <yingying.zhu@alumnos.upm.es>
2024-04-30 17:14:11 +0200

    CHECK

--- app/src/routes/items.js
+++ app/src/routes/items.js
@@ -10,5 +10,5 @@ router.get("/:id", ItemsController.items_get_item);
 //Only accesible with authorization
 router.post("/", checkAuth, ItemsController.items_create_item);
 router.delete("/:id", checkAuth, ItemsController.items_delete_item);
-router.put("/", checkAuth, ItemsController.items_update_item)
+router.put("/:id", checkAuth, ItemsController.items_update_item)
 module.exports = router;
